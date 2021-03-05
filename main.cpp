6c4aaaf800373aeff43853f426e7024294b11087
Codeissimple <adam.jkirjacev@gmail.com>
2021-03-05 00:40:40 +0000

    Puinctuation

--- main.cpp
+++ main.cpp
@@ -7,25 +7,25 @@ int main() {
   cin >> grade;
 
   if(grade < 0 || grade > 100)
-  cout << "The grade is invalid." << endl ;
+  cout << "The grade is invalid." << endl;
 
   if(grade == 100)
-  cout << "Wow, you're good!" << endl ;
+  cout << "Wow, you're good!" << endl;
 
   if(grade >= 90 && grade <= 99)
-  cout << "The grade is an A." << endl ;
+  cout << "The grade is an A." << endl;
 
   if(grade >= 80 && grade <= 89)
-  cout << "The grade is a B." << endl ;
+  cout << "The grade is a B." << endl;
 
   if(grade >= 70 && grade <= 79)
-  cout << "The grade is a C." << endl ;
+  cout << "The grade is a C." << endl;
 
   if(grade >= 65 && grade <= 69)
-  cout << "The grade is a D." << endl ;
+  cout << "The grade is a D." << endl;
 
   if(grade < 65)
-  cout << "The grade is failing" << endl ;
+  cout << "The grade is failing" << endl;
 
   return 0;
 }
